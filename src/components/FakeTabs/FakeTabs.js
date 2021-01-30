66c074131ab103aadc79c0c74770de991bf46189
illia <illia@pdx.edu>
2021-01-30 14:18:54 -0800

    upd

---
+++ src/components/FakeTabs/FakeTabs.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const FakeTabs = () => {
+
+};
+
+export default FakeTabs;
---
+++ src/components/FakeTabs/index.js
@@ -0,0 +1,2 @@
+export { default } from './FakeTabs';
+export { default as FakeTabs } from './FakeTabs';

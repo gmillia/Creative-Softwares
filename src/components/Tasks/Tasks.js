5e22826625809266f95b081a251d741aaba9ab2a
illia <illia@pdx.edu>
2021-01-30 21:52:20 -0800

    removed unused

--- src/components/Tasks/Tasks.js
+++ src/components/Tasks/Tasks.js
@@ -5,33 +5,17 @@ import PropTypes from 'prop-types';
 import { useLocalStorage } from 'hooks';
 
 //COMPONENTS
-import { TaskHead, TaskPriority, Task } from 'components';
+import { Task } from 'components';
 
 //MATERIAL UI
-import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
-import Accordion from '@material-ui/core/Accordion';
-import AccordionDetails from '@material-ui/core/AccordionDetails';
-import AccordionSummary from '@material-ui/core/AccordionSummary';
 
-const useStyles = makeStyles(theme => ({
-    expanded: {
-        margin: "5px auto !important"
-    },
-    content: {
-        transition: 'none !important'
-    },
-    details: {
-        padding: '0px 16px 16px 16px'
-    }
-}))
 /**
  * Component which display Tasks based on their completion. That is, it filters tasks using pending param. 
  * 
  * @param {Boolean} pending Specifies whether to filter tasks which are currently not completed or not.  
  */
 const Tasks = ({ pending=false }) => {
-    const classes = useStyles();
     const { tasks } = useLocalStorage();
     const filteredTasks = tasks.filter(obj => pending ? !obj.completed : obj.completed);
     const [expanded, setExpanded] = useState(false);

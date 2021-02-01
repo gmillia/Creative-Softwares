7fdfddc300f908a50de64b053241927e91055609
illia <illia@pdx.edu>
2021-02-01 11:55:31 -0800

    Tooltip change

--- src/components/parts/CompleteTask/CompleteTask.js
+++ src/components/parts/CompleteTask/CompleteTask.js
@@ -37,7 +37,7 @@ const CompleteTask = ({ task }) => {
 
     return (
         <div>
-            <Tooltip title='Complete task' >
+            <Tooltip title={completed ? 'Uncomplete Task' : 'Complete task'} >
                 <Checkbox checked={completed} className={classes.checkBox} onClick={handleCheckBoxClick} />
             </Tooltip>
         </div>
